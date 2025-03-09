dcf2314fc9a007660f3dd0039965eac7d3cbc88f
Dawid Smulewicz <dawid@necodeo.com>
2025-03-09 12:14:52 +0100

    help & exit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strings"
 )
@@ -13,20 +14,71 @@ func cleanInput(text string) []string {
 	return strings.Fields(text) // split the text into words
 }
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Printf("Usage:\n\n")
+	for _, command := range commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
+
+var commands map[string]cliCommand
+
 func main() {
+	// define the commands
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+
 	// wait for user input
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		print("Pokedex > ")
+		fmt.Print("Pokedex > ")
 
 		// read the input
 		scanner.Scan()
 		input := scanner.Text()
 
+		// clean the input
 		words := cleanInput(input)
-		if len(words) > 0 {
-			print("Your command was: " + words[0] + "\n")
+		if len(words) == 0 {
+			continue
+		}
+
+		// check if the command exists
+		command, ok := commands[words[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		// execute the command
+		err := command.callback()
+		if err != nil {
+			fmt.Println("Error executing command:", err)
 		}
 	}
 }
