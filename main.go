900b7b4272e432bf75019c825ab803771f70e7e6
Dawid Smulewicz <dawid@necodeo.com>
2025-03-09 12:52:14 +0100

    pokeapi

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -17,22 +21,100 @@ func cleanInput(text string) []string {
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
-func commandExit() error {
+func commandExit(config *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(config *config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Printf("Usage:\n\n")
 	for _, command := range commands {
 		fmt.Printf("%s: %s\n", command.name, command.description)
 	}
 	return nil
+}
+
+type LocationAreaResponse struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func getLocationAreas(url string) (*LocationAreaResponse, error) {
+	if url == "" {
+		url = "https://pokeapi.co/api/v2/location-area"
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("unexpected status code: %d", res.StatusCode)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data LocationAreaResponse
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &data, nil
+}
+
+func commandMap(config *config) error {
+	data, err := getLocationAreas(config.Next)
+	if err != nil {
+		return err
+	}
+
+	config.Next = data.Next
+	config.Previous = data.Previous
+
+	for _, result := range data.Results {
+		fmt.Println(result.Name)
+	}
+
+	return nil
+}
+
+func commandMapBack(config *config) error {
+	if config.Previous == "" {
+		fmt.Println("you're on the first page")
+	}
+
+	data, err := getLocationAreas(config.Previous)
+	if err != nil {
+		return err
+	}
+
+	config.Next = data.Next
+	config.Previous = data.Previous
+
+	for _, result := range data.Results {
+		fmt.Println(result.Name)
+	}
+
+	return nil
+}
+
+type config struct {
+	Next     string
+	Previous string
 }
 
 var commands map[string]cliCommand
@@ -50,10 +132,22 @@ func main() {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays the location areas",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the previous page of location areas",
+			callback:    commandMapBack,
+		},
 	}
 
 	// wait for user input
 	scanner := bufio.NewScanner(os.Stdin)
+
+	cfg := config{}
 
 	for {
 		fmt.Print("Pokedex > ")
@@ -76,7 +170,7 @@ func main() {
 		}
 
 		// execute the command
-		err := command.callback()
+		err := command.callback(&cfg)
 		if err != nil {
 			fmt.Println("Error executing command:", err)
 		}
