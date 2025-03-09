7ee2a4cbfed6f1916a33408ebfd43bddf73d5b94
Dawid Smulewicz <dawid@necodeo.com>
2025-03-09 14:59:59 +0100

    catch command

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"os"
 	"strings"
@@ -184,6 +185,146 @@ func getLocationArea(area string, cache *pokecache.Cache) (*LocationAreaResponse
 	return &data, nil
 }
 
+type PokemonResponse struct {
+	ID                     int              `json:"id"`
+	Name                   string           `json:"name"`
+	BaseExperience         int              `json:"base_experience"`
+	Height                 int              `json:"height"`
+	IsDefault              bool             `json:"is_default"`
+	Order                  int              `json:"order"`
+	Weight                 int              `json:"weight"`
+	Abilities              []Ability        `json:"abilities"`
+	Forms                  []Form           `json:"forms"`
+	GameIndices            []GameIndex      `json:"game_indices"`
+	HeldItems              []HeldItem       `json:"held_items"`
+	LocationAreaEncounters string           `json:"location_area_encounters"`
+	Moves                  []Move           `json:"moves"`
+	Species                NamedAPIResource `json:"species"`
+	Sprites                Sprites          `json:"sprites"`
+	Cries                  Cries            `json:"cries"`
+	Stats                  []Stat           `json:"stats"`
+	Types                  []TypeSlot       `json:"types"`
+	PastTypes              []PastType       `json:"past_types"`
+}
+
+type Ability struct {
+	IsHidden bool             `json:"is_hidden"`
+	Slot     int              `json:"slot"`
+	Ability  NamedAPIResource `json:"ability"`
+}
+
+type Form struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type GameIndex struct {
+	GameIndex int              `json:"game_index"`
+	Version   NamedAPIResource `json:"version"`
+}
+
+type HeldItem struct {
+	Item           NamedAPIResource `json:"item"`
+	VersionDetails []VersionDetail  `json:"version_details"`
+}
+
+type VersionDetail struct {
+	Rarity  int              `json:"rarity"`
+	Version NamedAPIResource `json:"version"`
+}
+
+type Move struct {
+	Move                NamedAPIResource     `json:"move"`
+	VersionGroupDetails []VersionGroupDetail `json:"version_group_details"`
+}
+
+type VersionGroupDetail struct {
+	LevelLearnedAt  int              `json:"level_learned_at"`
+	VersionGroup    NamedAPIResource `json:"version_group"`
+	MoveLearnMethod NamedAPIResource `json:"move_learn_method"`
+}
+
+type NamedAPIResource struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type Sprites struct {
+	FrontDefault string       `json:"front_default"`
+	FrontShiny   string       `json:"front_shiny"`
+	Other        OtherSprites `json:"other"`
+}
+
+type OtherSprites struct {
+	DreamWorld      ImageResource `json:"dream_world"`
+	Home            ImageResource `json:"home"`
+	OfficialArtwork ImageResource `json:"official-artwork"`
+	Showdown        ImageResource `json:"showdown"`
+}
+
+type ImageResource struct {
+	FrontDefault string `json:"front_default"`
+	FrontShiny   string `json:"front_shiny"`
+}
+
+type Cries struct {
+	Latest string `json:"latest"`
+	Legacy string `json:"legacy"`
+}
+
+type Stat struct {
+	BaseStat int              `json:"base_stat"`
+	Effort   int              `json:"effort"`
+	Stat     NamedAPIResource `json:"stat"`
+}
+
+type TypeSlot struct {
+	Slot int              `json:"slot"`
+	Type NamedAPIResource `json:"type"`
+}
+
+type PastType struct {
+	Generation NamedAPIResource `json:"generation"`
+	Types      []TypeSlot       `json:"types"`
+}
+
+func getPokemon(pokemon string, cache *pokecache.Cache) (*PokemonResponse, error) {
+	url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", pokemon)
+
+	if cachedData, found := cache.Get(url); found {
+		var cachedResponse PokemonResponse
+		err := json.Unmarshal(cachedData, &cachedResponse)
+		if err != nil {
+			return nil, err
+		}
+		return &cachedResponse, nil
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("error: %d", res.StatusCode)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	var data PokemonResponse
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	cache.Add(url, body)
+
+	return &data, nil
+}
+
 func commandMap(config *config, args []string) error {
 	data, err := getLocationAreas(config.Next, config.Cache)
 	if err != nil {
@@ -247,10 +388,46 @@ func commandExplore(config *config, args []string) error {
 	return nil
 }
 
+func commandCatch(config *config, args []string) error {
+	if len(args) != 1 {
+		fmt.Println("Usage: catch <pokemon>")
+		return nil
+	}
+
+	pokemon := args[0]
+
+	fmt.Printf("Throwing a Pokeball at %s...\n", pokemon)
+
+	data, err := getPokemon(pokemon, config.Cache)
+	if err != nil {
+		fmt.Println("Error catching the Pokémon:", err)
+		return nil
+	}
+
+	// max experience is 608
+	// https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_effort_value_yield_in_Generation_IX
+	catchRate := float64(data.BaseExperience) / 608.0
+	if catchRate > 1.0 { // we make sure the base experience is not higher than 608
+		catchRate = 1.0
+	}
+
+	randomNumber := rand.Float64()
+	if randomNumber > catchRate {
+		fmt.Printf("%s was caught!\n", pokemon)
+
+		config.Pokemon[pokemon] = *data
+	} else {
+		fmt.Printf("%s escaped!\n", pokemon)
+	}
+
+	return nil
+}
+
 type config struct {
 	Next     string
 	Previous string
 	Cache    *pokecache.Cache
+	Pokemon  map[string]PokemonResponse // for now we use PokemonResponse but i don't think we need all the data
 }
 
 var commands map[string]cliCommand
@@ -283,13 +460,19 @@ func main() {
 			description: "Displays the Pokémon located in a location area",
 			callback:    commandExplore,
 		},
+		"catch": {
+			name:        "catch",
+			description: "Catches a Pokémon",
+			callback:    commandCatch,
+		},
 	}
 
 	// wait for user input
 	scanner := bufio.NewScanner(os.Stdin)
 
 	cfg := config{
-		Cache: pokecache.NewCache(10 * time.Second), // create a new cache with a 10 second interval
+		Cache:   pokecache.NewCache(10 * time.Second), // create a new cache with a 10 second interval
+		Pokemon: make(map[string]PokemonResponse),     // create a map to store the caught Pokémon
 	}
 
 	for {
@@ -311,9 +494,10 @@ func main() {
 			fmt.Println("Unknown command")
 			continue
 		}
+		args := words[1:]
 
 		// execute the command
-		err := command.callback(&cfg, words[1:])
+		err := command.callback(&cfg, args)
 		if err != nil {
 			fmt.Println("Error executing command:", err)
 		}
